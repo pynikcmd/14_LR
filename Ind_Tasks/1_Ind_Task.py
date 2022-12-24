af541fef8598241a858b632a0ab8203d9d9d6de8
pynikacmd <ver.r762@gmail.com>
2022-12-24 14:10:37 +0300

    Update 1_Ind_Task.py

--- Ind_Tasks/1_Ind_Task.py
+++ Ind_Tasks/1_Ind_Task.py
@@ -5,16 +5,32 @@
 def typer(types="even"):
     def dele(*args):
         values = [int(arg) for arg in args]
-        # удаление четных
+        i = 0
+
+        # Удаление четных.
         if types == 'even':
-            values = [itm for itm in values if itm % 2 != 0]
-        # удаление нечетных
+            for j in values[:]:
+                print(j)
+                if j % 2 == 0:
+                    print('deleting chet', i)
+                    del values[i]
+                else:
+                    i += 1
+
+        # Удаление нечетных.
         else:
-            values = [itm for itm in values if itm % 2 == 0]
+            for j in values[:]:
+                print(j)
+                if j % 2 != 0:
+                    print('deleting chet', i)
+                    del values[i]
+                else:
+                    i += 1
+
         return values
 
     return dele
 
 
 if __name__ == '__main__':
-    print(*typer("noteven")(2, 9, 7, 8, 4, 2, 15))
+    print(*typer("deven")(2, 9, 7, 8, 4, 2, 15))
